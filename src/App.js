0c18d248f905b4381e73c7966f5872521e0f2335
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-03-20 03:07:55 +0600

    incrise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCount></LoadCount>
     </div>
   );
 }
+
+
+function LoadCount(){
+  const [count, setCount] = useState(0)
+  const Increse=()=>setCount(count+1);
+  const Decrese=()=>setCount(count-1);
+  return (
+    <div>
+      <h1>Increase or decrise a Number:</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={Increse}>Increse</button>
+      <button onClick={Decrese}>Decrese</button>
+    </div>
+  )
+}
+
+
 
 export default App;
